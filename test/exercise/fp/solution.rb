cae3b7765e830651dd8d0b8db3dddbabe7dab54d
artlessnick <artlessnick@gmail.com>
2021-08-17 23:14:36 +0300

    Added def chars_count

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -12,8 +12,12 @@ module Exercise
         new_array.map { |el| el['rating_kinopoisk'].to_f }.reduce(:+) / new_array.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        array = films.select { |el| el['rating_kinopoisk'].to_f >= threshold }
+
+        result_map = array.map { |el| el['name'] }
+
+        result_map.reduce(0) { |sum, n| n.count('и') != 0 ? sum + n.count('и') : sum }
       end
     end
   end
