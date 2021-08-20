e01e173ad2cc04527b736e10e53fce1be2abfb7b
artlessnick <artlessnick@gmail.com>
2021-08-20 13:33:50 +0300

    Added all methods in fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,49 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        index = 0
+
+        while index < length
+          yield self[index]
+          index += 1
+        end
+
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = MyArray.new
+
+        my_each do |el|
+          result.push(yield el)
+        end
+
+        result
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = MyArray.new
+
+        my_each do |el|
+          result.push(el) unless el.nil?
+        end
+
+        result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(init = nil)
+        acc = self[0]
+
+        slice(1..(length - 1)).my_each do |el|
+          acc = yield acc, el
+        end
+
+        init.nil? ? acc : yield(init, acc)
+      end
     end
   end
 end
