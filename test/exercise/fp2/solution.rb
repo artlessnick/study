b6ab9994b9988594c10e304d412974669ab85ed1
artlessnick <artlessnick@gmail.com>
2021-08-21 13:23:48 +0300

    fixed all methods in fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,47 +6,51 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
-        index = 0
+        recursive_yield = lambda { |array|
+          unless array.empty?
+            yield array[0]
+            recursive_yield.call array[1..-1]
+          end
+        }
 
-        while index < length
-          yield self[index]
-          index += 1
-        end
-
+        recursive_yield.call self
         self
       end
 
       # Написать свою функцию my_map
       def my_map
-        result = MyArray.new
-
-        my_each do |el|
+        my_reduce(MyArray.new) do |result, el|
           result.push(yield el)
+          result
         end
-
-        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
-        result = MyArray.new
-
-        my_each do |el|
+        my_reduce(MyArray.new) do |result, el|
           result.push(el) unless el.nil?
+          result
         end
-
-        result
       end
 
       # Написать свою функцию my_reduce
       def my_reduce(init = nil)
-        acc = self[0]
+        acc = init.nil? ? self[0] : init
 
-        slice(1..(length - 1)).my_each do |el|
-          acc = yield acc, el
+        recursive_yield = lambda { |array|
+          unless array.empty?
+            acc = yield acc, array[0]
+            recursive_yield.call array[1..-1]
+          end
+        }
+
+        if init.nil?
+          recursive_yield.call self[1..-1]
+        else
+          recursive_yield.call self
         end
 
-        init.nil? ? acc : yield(init, acc)
+        acc
       end
     end
   end
